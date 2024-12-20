7179d549816528eb7d1599876c5b65c926065ea2
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-12-21 04:22:31 +0845

    Added a page for lists

--- lib/screens/book_listing.dart
+++ lib/screens/book_listing.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:redi/constants/constants.dart';
 
 class BookListing extends StatelessWidget {
   const BookListing({super.key});
@@ -6,9 +8,45 @@ class BookListing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Lists Coming Soon'),
-      ),
-    );
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            children: [
+              Expanded(child: SizedBox()),
+              Expanded(child: SizedBox()),
+              Expanded(child: SizedBox()),
+              Lottie.asset(
+                'assets/animations/book-loading.json',
+                width: AppMeasure.width / 1.5,
+              ),
+              Text(
+                'Lists Coming Soon',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              Expanded(child: SizedBox()),
+              Expanded(child: SizedBox()),
+              GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                    color: Colors.black,
+                    child: Text(
+                      'Go Back',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(child: SizedBox()),
+            ],
+          ),
+        ));
   }
 }
