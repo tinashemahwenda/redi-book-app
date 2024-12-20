1add08a2ad3f226bb2f9f9c0c47406fc0577dae3
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-12-21 04:07:36 +0845

    Added a lottie animation to assets and created a view for listing

---
+++ lib/screens/book_listing.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BookListing extends StatelessWidget {
+  const BookListing({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Lists Coming Soon'),
+      ),
+    );
+  }
+}
