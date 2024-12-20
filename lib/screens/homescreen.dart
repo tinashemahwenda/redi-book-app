8eebc0156a7035135f2d1d9f62a2a0c9239ef41c
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-12-21 03:23:11 +0845

    Removed unnecesary imports

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-
-import 'package:emoji_data/emoji_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:redi/components/bubble_tile.dart';
