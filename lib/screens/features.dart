7491922db0d70a946ce8ad31b4f617349b076214
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-12-22 01:26:00 +0845

    Created a simple view for features page

--- lib/screens/features.dart
+++ lib/screens/features.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class FeaturesPage extends StatelessWidget {
+  const FeaturesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text('Upcoming Updates'),
+            ListTile(
+              leading: CircleAvatar(
+                child: Icon(Icons.check),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
