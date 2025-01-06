36093dc764f4544ddd608874c43194dd814f05b7
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-06 23:53:40 +0200

    Made a text size customisatin feature

--- lib/components/settingsmodal.dart
+++ lib/components/settingsmodal.dart
@@ -14,10 +14,85 @@ class SettingsModal extends StatelessWidget {
         height: AppMeasure.height / 3,
         color: Colors.white,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Themes & Settings',
-              style: TextStyle(fontFamily: 'Garomond-Bold'),
+            SizedBox(height: 20),
+            Center(
+              child: Text(
+                'Themes & Settings',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Text Size',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    spacing: 10,
+                    children: [
+                      Container(
+                        //padding: EdgeInsets.all(10),
+                        width: AppMeasure.width / 10,
+                        height: AppMeasure.height / 20,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.black,
+                            )),
+                        child: Center(
+                            child: Text(
+                          'A',
+                          style: TextStyle(
+                            fontSize: 10,
+                          ),
+                        )),
+                      ),
+                      Container(
+                        //padding: EdgeInsets.all(10),
+                        width: AppMeasure.width / 10,
+                        height: AppMeasure.height / 20,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.black,
+                            )),
+                        child: Center(child: Text('A')),
+                      ),
+                      Container(
+                        width: AppMeasure.width / 10,
+                        height: AppMeasure.height / 20,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.black,
+                            )),
+                        child: Center(
+                            child: Text(
+                          'A',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        )),
+                      )
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
