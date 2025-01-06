a4f6c9cdad2c73106897f2b16ec671ef4cf7b640
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-07 00:10:30 +0200

    Added a brightness component

--- lib/components/settingsmodal.dart
+++ lib/components/settingsmodal.dart
@@ -11,31 +11,46 @@ class SettingsModal extends StatelessWidget {
       borderRadius: BorderRadius.circular(40),
       child: Container(
         width: AppMeasure.width,
-        height: AppMeasure.height / 3,
+        height: AppMeasure.height / 2,
         color: Colors.white,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 20),
-            Center(
-              child: Text(
-                'Themes & Settings',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Center(
+                child: Text(
+                  'Themes & Settings',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.only(left: 30.0),
-              child: Column(
+              SizedBox(height: 20),
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  Text(
+                    'Brightness',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Container(
+                    child: Slider(
+                      activeColor: Colors.black,
+                      value: 0,
+                      onChanged: (value) => 100,
+                    ),
+                  ),
                   Text(
                     'Text Size',
                     style: TextStyle(
                       fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                   SizedBox(height: 10),
@@ -93,8 +108,90 @@ class SettingsModal extends StatelessWidget {
                   )
                 ],
               ),
-            )
-          ],
+              SizedBox(height: 20),
+              Text(
+                'Font Family',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                spacing: 10,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.black,
+                        )),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Aa',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Text('Redi Default'),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.black,
+                        )),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Aa',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            fontFamily: '',
+                          ),
+                        ),
+                        Text(
+                          'Sans Default',
+                          style: TextStyle(fontFamily: ''),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.black,
+                        )),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Aa',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Text('Redi Default'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
