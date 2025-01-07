42ab198b0209306ee3fef6953a5fb2f2320ac3a0
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-07 19:41:10 +0200

    Added a feature to increase font size

--- lib/screens/chapter_page.dart
+++ lib/screens/chapter_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:redi/components/chapter_tile.dart';
+import 'package:screen_brightness/screen_brightness.dart';
 
-import '../components/settingsmodal.dart';
 import '../constants/constants.dart';
 import '../models/book.dart';
 
@@ -14,11 +14,235 @@ class ChapterPage extends StatefulWidget {
 }
 
 class _ChapterPageState extends State<ChapterPage> {
+  double textSize = 20;
+  double _brightness = 0.5;
+  void increaseFontSize() {
+    setState(() {
+      textSize = 28;
+      print('Text Size: $textSize');
+    });
+  }
+
+  void decreaseFontSize() {
+    setState(() {
+      textSize = 14;
+      print('Text Size: $textSize');
+    });
+  }
+
+  void defaultFontSize() {
+    setState(() {
+      textSize = 20;
+      print('Text Size: $textSize');
+    });
+  }
+
   void showSettingsModal() {
     showModalBottomSheet(
         context: context,
         builder: (BuildContext context) {
-          return SettingsModal();
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(40),
+            child: Container(
+              width: AppMeasure.width,
+              height: AppMeasure.height / 2,
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 20),
+                    Center(
+                      child: Text(
+                        'Themes & Settings',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Brightness',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Container(
+                          child: Slider(
+                            activeColor: Colors.black,
+                            value: _brightness,
+                            min: 0,
+                            max: 1,
+                            onChanged: (value) => setState(() {
+                              _brightness = value;
+
+                              try {
+                                ScreenBrightness()
+                                    .setScreenBrightness(_brightness);
+                              } catch (e) {
+                                print('Error in slider $e');
+                              }
+                            }),
+                          ),
+                        ),
+                        Text(
+                          'Text Size',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          spacing: 10,
+                          children: [
+                            Container(
+                              //padding: EdgeInsets.all(10),
+                              width: AppMeasure.width / 10,
+                              height: AppMeasure.height / 20,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(
+                                    width: 1,
+                                    color: Colors.black,
+                                  )),
+                              child: Center(
+                                  child: Text(
+                                'A',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                ),
+                              )),
+                            ),
+                            Container(
+                              //padding: EdgeInsets.all(10),
+                              width: AppMeasure.width / 10,
+                              height: AppMeasure.height / 20,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(
+                                    width: 1,
+                                    color: Colors.black,
+                                  )),
+                              child: Center(child: Text('A')),
+                            ),
+                            GestureDetector(
+                              onTap: increaseFontSize,
+                              child: Container(
+                                width: AppMeasure.width / 10,
+                                height: AppMeasure.height / 20,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    border: Border.all(
+                                      width: 1,
+                                      color: Colors.black,
+                                    )),
+                                child: Center(
+                                    child: Text(
+                                  'A',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                  ),
+                                )),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Font Family',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      spacing: 10,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                width: 1,
+                                color: Colors.black,
+                              )),
+                          child: Column(
+                            children: [
+                              Text(
+                                'Aa',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Text('Redi Default'),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                width: 1,
+                                color: Colors.black,
+                              )),
+                          child: Column(
+                            children: [
+                              Text(
+                                'Aa',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  fontFamily: '',
+                                ),
+                              ),
+                              Text(
+                                'Sans Default',
+                                style: TextStyle(fontFamily: ''),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                width: 1,
+                                color: Colors.black,
+                              )),
+                          child: Column(
+                            children: [
+                              Text(
+                                'Aa',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Text('Redi Default'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ),
+            ),
+          );
         });
   }
 
@@ -55,6 +279,7 @@ class _ChapterPageState extends State<ChapterPage> {
                       itemCount: widget.book.chapters.length,
                       itemBuilder: (context, index) {
                         return ChapterTile(
+                          chapterSize: textSize,
                           currentChapter:
                               widget.book.chapters[index].chapter.toString(),
                           bookAuthor: widget.book.author,
