ca649077e4137f0a532b337f0b7a93ad46c58f9d
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-14 17:28:30 +0200

    Added a paragraph text

--- lib/screens/home_splash_screen.dart
+++ lib/screens/home_splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:redi/constants/constants.dart';
 
 class HomeSplashScreen extends StatelessWidget {
   const HomeSplashScreen({super.key});
@@ -7,12 +8,38 @@ class HomeSplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
         body: Column(
-      children: [
-        SvgPicture.asset(
-          'assets/images/splash.svg',
-        )
-      ],
-    ));
+          children: [
+            SizedBox(
+              height: 150,
+            ),
+            Center(
+              child: SvgPicture.asset(
+                'assets/images/splash.svg',
+                width: AppMeasure.width,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Where stories \nwait to be told',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                height: 1,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Indulge in the magic of storytelling through\n our Redi Books as our authors weave tales that will captivate, inspire & transform you!',
+                style: TextStyle(),
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ));
   }
 }
