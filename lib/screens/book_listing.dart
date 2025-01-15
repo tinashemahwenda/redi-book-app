4699050f9aaf20128eeedfffc5efba3484dfaf78
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-15 13:26:33 +0200

    Removed all the listings code

--- lib/screens/book_listing.dart
+++ lib/screens/book_listing.dart
@@ -9,42 +9,13 @@ class BookListing extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Center(
+        body: SingleChildScrollView(
           child: Column(
             children: [
-              Expanded(child: SizedBox()),
-              Expanded(child: SizedBox()),
-              Expanded(child: SizedBox()),
-              Lottie.asset(
-                'assets/animations/book-loading.json',
-                width: AppMeasure.width / 1.5,
+              SizedBox(
+                height: 20,
               ),
-              Text(
-                'Lists Coming Soon',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-              Expanded(child: SizedBox()),
-              Expanded(child: SizedBox()),
-              GestureDetector(
-                onTap: () => Navigator.pop(context),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                    color: Colors.black,
-                    child: Text(
-                      'Go Back',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(child: SizedBox()),
+              Row()
             ],
           ),
         ));
