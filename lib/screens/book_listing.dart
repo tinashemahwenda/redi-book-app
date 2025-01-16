fb8f1cc496962df463f302b9b1b03b6fc33e95eb
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-16 05:27:36 +0200

    Migrated code to a seperate file

--- lib/screens/book_listing.dart
+++ lib/screens/book_listing.dart
@@ -93,23 +93,6 @@ class BookListing extends StatelessWidget {
                 SizedBox(
                   height: 10,
                 ),
-                SizedBox(
-                  height: AppMeasure.height / 1.3,
-                  width: AppMeasure.width,
-                  child: GridView.builder(
-                    itemCount: 10,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2),
-                    itemBuilder: (context, index) => Padding(
-                      padding: const EdgeInsets.only(bottom: 8.0, left: 10),
-                      child: Container(
-                        width: 100,
-                        height: 40,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ),
-                )
               ],
             ),
           ),
