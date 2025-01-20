f9f4a40755e89acb99ce8cbf0f084098ee7a191c
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-20 18:20:45 +0200

    Created a cool navigation bar

---
+++ lib/components/cool_navigationbar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../screens/favorites_page.dart';
+
+class CoolNavigationbar extends StatelessWidget {
+  const CoolNavigationbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Icon(Icons.menu),
+        Row(
+          spacing: 10,
+          children: [
+            GestureDetector(
+              onTap: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => FavoritesPage())),
+              child: Icon(
+                Icons.favorite,
+                color: Colors.deepPurpleAccent,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                  border: Border.all(width: 1, color: Colors.black)),
+              child: Text(
+                'Leaderboard',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
