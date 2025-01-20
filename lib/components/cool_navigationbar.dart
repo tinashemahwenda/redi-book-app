ed6c01cea59ea4e9fa726543a4a777378a6060ef
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-20 18:42:35 +0200

    Added routing to leaderboard navigation

--- lib/components/cool_navigationbar.dart
+++ lib/components/cool_navigationbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:redi/screens/leaderboard.dart';
 import '../screens/favorites_page.dart';
 
 class CoolNavigationbar extends StatelessWidget {
@@ -23,16 +24,20 @@ class CoolNavigationbar extends StatelessWidget {
                 color: Colors.deepPurpleAccent,
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
-                  border: Border.all(width: 1, color: Colors.black)),
-              child: Text(
-                'Leaderboard',
-                style: TextStyle(
-                  color: Colors.black,
+            GestureDetector(
+              onTap: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => LeadboardScreen())),
+              child: Container(
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    border: Border.all(width: 1, color: Colors.black)),
+                child: Text(
+                  'Leaderboard',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
                 ),
               ),
             )
