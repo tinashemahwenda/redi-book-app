797e0e6e816f32c39b0aa257456e5b95ac718560
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-22 06:24:11 +0200

    Styled the stats box in the profile

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -51,7 +51,76 @@ class ProfilePage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(height: 20)
+                  SizedBox(height: 40),
+                  Container(
+                    width: AppMeasure.width,
+                    height: AppMeasure.height / 8,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          spacing: 10,
+                          children: [
+                            Icon(
+                              Icons.book_outlined,
+                              color: Colors.deepPurple,
+                            ),
+                            Text(
+                              '20 books',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(
+                              'saved',
+                              style: TextStyle(
+                                height: 0,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          spacing: 10,
+                          children: [
+                            Icon(
+                              Icons.timer_outlined,
+                              color: Colors.deepPurple,
+                            ),
+                            Text(
+                              '3hrs ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text('read time'),
+                          ],
+                        ),
+                        Column(
+                          spacing: 10,
+                          children: [
+                            Icon(
+                              Icons.check_circle_outline_rounded,
+                              color: Colors.deepPurple,
+                            ),
+                            Text(
+                              '12 books ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text('completed'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    thickness: 1,
+                    color: Colors.grey[300],
+                  )
                 ],
               ),
             )
