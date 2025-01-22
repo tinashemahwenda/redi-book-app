0e669b0b3893723d761d5ff90d4c87deb8d2ca11
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-22 06:49:47 +0200

    Rounded up the design

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -17,6 +17,9 @@ class ProfilePage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              SizedBox(
+                height: 30,
+              ),
               GoBackButton(),
               SizedBox(
                 height: 30,
