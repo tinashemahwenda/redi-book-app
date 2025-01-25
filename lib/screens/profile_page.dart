63f05ef36110104812f2b446fde7eaa8c8a444da
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-25 15:00:38 +0200

    Fixed overflow issues on the profile page

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -13,83 +13,79 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+      body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 30,
-              ),
-              GoBackButton(),
-              SizedBox(
-                height: 30,
-              ),
-              Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.grey[300],
-                      child: Image.asset(
-                        'assets/images/profile/face.png',
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GoBackButton(),
+                Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.grey[300],
+                        child: Image.asset(
+                          'assets/images/profile/face.png',
+                        ),
+                        radius: 80,
                       ),
-                      radius: 80,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      'Tinashe',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
+                      SizedBox(
+                        height: 20,
                       ),
-                    ),
-                    Container(
-                      width: AppMeasure.width / 1.5,
-                      child: Center(
-                        child: Text(
-                          'Young explosive reader. He finished 10 in a month🔥',
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                          textAlign: TextAlign.center,
+                      Text(
+                        'Tinashe',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
                         ),
                       ),
-                    ),
-                    SizedBox(height: 40),
-                    StatsBox(),
-                    Divider(
-                      thickness: 1,
-                      color: Colors.grey[300],
-                    ),
-                    SizedBox(height: 20),
-                    SettingsTile(
-                      iconName: Icons.notifications_none,
-                      settingTileName: 'Notifications',
-                      redColor: false,
-                    ),
-                    SettingsTile(
-                      iconName: Icons.settings_suggest_outlined,
-                      settingTileName: 'Theme Settings',
-                      redColor: false,
-                    ),
-                    SettingsTile(
-                      iconName: Icons.bookmark_border_outlined,
-                      settingTileName: 'Saved Books',
-                      redColor: false,
-                    ),
-                    SettingsTile(
-                      iconName: Icons.delete_outline_rounded,
-                      settingTileName: 'Clear Profile',
-                      redColor: true,
-                    ),
-                  ],
-                ),
-              )
-            ],
+                      Container(
+                        width: AppMeasure.width / 1.5,
+                        child: Center(
+                          child: Text(
+                            'Young explosive reader. He finished 10 in a month🔥',
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      StatsBox(),
+                      Divider(
+                        thickness: 1,
+                        color: Colors.grey[300],
+                      ),
+                      SizedBox(height: 20),
+                      SettingsTile(
+                        iconName: Icons.notifications_none,
+                        settingTileName: 'Notifications',
+                        redColor: false,
+                      ),
+                      SettingsTile(
+                        iconName: Icons.settings_suggest_outlined,
+                        settingTileName: 'Theme Settings',
+                        redColor: false,
+                      ),
+                      SettingsTile(
+                        iconName: Icons.bookmark_border_outlined,
+                        settingTileName: 'Saved Books',
+                        redColor: false,
+                      ),
+                      SettingsTile(
+                        iconName: Icons.delete_outline_rounded,
+                        settingTileName: 'Clear Profile',
+                        redColor: true,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
