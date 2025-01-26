8586a2be4855dcdc4195150ce15eaa271fd2d75b
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-26 13:31:38 +0200

    Added a text for user readability

--- lib/screens/theme_settings_page.dart
+++ lib/screens/theme_settings_page.dart
@@ -7,15 +7,41 @@ class ThemeSettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(height: 30),
-            GoBackButton(),
-            Text('Theme Settings'),
-            SizedBox(height: 20),
-            Image.asset('assets/images/theme.png'),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 30),
+              GoBackButton(),
+              SizedBox(height: 20),
+              Text(
+                'Theme Settings',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Spacer(),
+              Image.asset('assets/images/theme.png'),
+              SizedBox(height: 10),
+              Center(
+                child: Text(
+                  'You can customize your theme here!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Spacer(),
+            ],
+          ),
         ),
       ),
     );
