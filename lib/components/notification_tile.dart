1106ade6cf26bed5257ff3351b4c64398b9484b6
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-26 12:32:13 +0200

    Created a seperate file to hold the notifications tile widget

---
+++ lib/components/notification_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotificationTile extends StatelessWidget {
+  final String notificationName;
+  final String notificationTime;
+  const NotificationTile(
+      {super.key,
+      required this.notificationName,
+      required this.notificationTime});
+
+  @override
+  Widget build(BuildContext context) {
+    return;
+  }
+}
