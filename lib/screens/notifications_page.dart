7f9e695a93af8cebc883eb07c00a8d5c1eafbab8
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-26 11:09:44 +0200

    Converted the main widget to stateful to keep the state of the switch

--- lib/screens/notifications_page.dart
+++ lib/screens/notifications_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:redi/components/back_button.dart';
 
-class NotificationsPage extends StatelessWidget {
+class NotificationsPage extends StatefulWidget {
   const NotificationsPage({super.key});
 
+  @override
+  State<NotificationsPage> createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  bool _selectedValue = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +31,56 @@ class NotificationsPage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              SizedBox(height: 40),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      spacing: 20,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.deepPurpleAccent[900],
+                          radius: 30,
+                          child: Icon(
+                            Icons.announcement,
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Alerts',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Text(_selectedValue
+                                ? 'Turn off some notifications'
+                                : 'Turn on some notifications')
+                          ],
+                        )
+                      ],
+                    ),
+                    Switch(
+                      value: _selectedValue,
+                      onChanged: (bool newValue) {
+                        setState(() {
+                          _selectedValue = newValue;
+                        });
+                      },
+                      thumbColor: WidgetStatePropertyAll(Colors.white),
+                      inactiveThumbColor: Colors.white,
+                      inactiveTrackColor: Colors.grey[300],
+                      trackOutlineColor:
+                          WidgetStatePropertyAll(Colors.grey[300]),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(child: SizedBox()),
+              Image.asset('assets/images/fiction.png'),
+              Expanded(child: SizedBox())
             ],
           ),
         ),
