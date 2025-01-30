21de304952f26b4e60f5cb1240f8b0e02328baa8
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-30 14:35:31 +0200

    Removed unnecesary color debug

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -40,7 +40,6 @@ class _HomePageState extends State<HomePage> {
         _books = books;
 
         print(Colors.deepPurple.hashCode);
-        print(Colors.white30.value);
       });
     } catch (e) {
       print('Error in loading data $e');
